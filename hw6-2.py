86ce415f8bd0baae0423213fb9aec5b71d726a17
p22npiselli <p22npiselli@fairfieldprep.org>
2021-11-15 10:21:05 -0500

    hw 6-2

---
+++ hw6-2.py
@@ -0,0 +1,33 @@
+# Author: Nolan (AMDG) 10/29/2021
+
+# Question 1
+my_lst1 = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(my_lst1))
+
+# Question 3
+print(my_lst1[2])
+
+# Question 4
+print(5 not in my_lst1)
+
+# Question 5
+print(10 not in my_lst1)
+
+# Question 6
+name_lst = ["Chris", "Nolan", "Ryan"]
+print(name_lst)
+
+# Question 7
+print(name_lst[1:2])
+
+# Question 8
+print(name_lst * 3)
+
+# Question 9
+name_lst2 = my_lst1 + name_lst
+print(name_lst2)
+
+# Question 10
+print(len(name_lst2))
